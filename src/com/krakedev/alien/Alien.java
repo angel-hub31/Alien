d5ed3d74063e029918458b5a6018d177a6cf5e5f
angel-hub31 <moralesmales1@hotmail.com>
2026-04-17 18:31:26 -0500

    contructores y metodos get

--- src/com/krakedev/alien/Alien.java
+++ src/com/krakedev/alien/Alien.java
@@ -9,6 +9,64 @@ public class Alien {
 	private double precioExtremidad;
 	private double precioOjo;
 	private double precioCuerpo;
+	private double precioTotal;
+	
+	
+	public Alien(int tamanio, String color) {
+	
+		if (tamanio < 5) {
+            this.tamanio = 5;
+        } else if (tamanio > 30) {
+            this.tamanio = 30;
+        } else {
+            this.tamanio = tamanio;
+	}
+		this.color = color;
+		
+		this.numeroOjos = 0;
+        this.numeroBrazos = 0;
+        this.numeroPies = 0;
+        this.precioTotal = 0.0;
+        
+        this.precioCuerpo = this.tamanio * 0.20;
+        this.precioExtremidad = this.tamanio * 0.10;
+        this.precioOjo = this.tamanio * 0.05;
+        
+       
+	
+	}
+
+
+	public int getTamanio() {
+		return tamanio;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getNumeroOjos() {
+		return numeroOjos;
+	}
+	public int getNumeroBrazos() {
+		return numeroBrazos;
+	}
+	public int getNumeroPies() {
+		return numeroPies;
+	}
+	public double getPrecioExtremidad() {
+		return precioExtremidad;
+	}
+	public double getPrecioOjo() {
+		return precioOjo;
+	}
+	public double getPrecioCuerpo() {
+		return precioCuerpo;
+	}
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+
+	
 	
 
 }
